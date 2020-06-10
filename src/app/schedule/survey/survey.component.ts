a808e9fc0aa7f71e768960561a5248f75029e217
Rachna Khokhar <rachnakhokhar@gmail.com>
2020-06-10 12:52:55 +0530

    API Fixes

--- src/app/schedule/survey/survey.component.ts
+++ src/app/schedule/survey/survey.component.ts
@@ -196,7 +196,7 @@ export class SurveyComponent implements OnInit, OnDestroy {
   }
 
   getAssignees() {
-    this.apiService.getSurveyors(UserRoles.SURVEYOR).subscribe(assignees => {
+    this.apiService.getSurveyors().subscribe(assignees => {
       this.listOfAssignees = [];
       assignees.forEach(item => this.listOfAssignees.push(item));
       console.log(this.listOfAssignees);
