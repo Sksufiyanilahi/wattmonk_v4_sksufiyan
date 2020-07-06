a9730d746aba019fd8a0489b03218721f1cccca7
Rachna Khokhar <rachnakhokhar@gmail.com>
2020-07-06 13:29:49 +0530

    Fixed cometchat issue

--- src/app/surveyoroverview/surveyoroverview.page.ts
+++ src/app/surveyoroverview/surveyoroverview.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CometChat } from '@cometchat-pro/cordova-ionic-chat';
+import { StorageService } from '../storage.service';
+import { COMET_CHAT_AUTH_KEY, COMET_CHAT_APP_ID, COMET_CHAT_REGION } from '../model/constants';
+import { ApiService } from '../api.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-surveyoroverview',
@@ -7,9 +13,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SurveyoroverviewPage implements OnInit {
 
-  constructor() { }
+  private subscription: Subscription;
+
+  constructor(public route: Router,
+    private storage: StorageService,
+    private apiService: ApiService) { }
 
   ngOnInit() {
+    this.setupCometChatUser();
+    this.updateUserPushToken();
+    this.route.navigate(['surveyoroverview/newsurveys']);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  setupCometChatUser() {
+    const appSetting = new CometChat.AppSettingsBuilder().subscribePresenceForAllUsers().setRegion(COMET_CHAT_REGION).build();
+    CometChat.init(COMET_CHAT_APP_ID, appSetting).then(
+      () => {
+        console.log('Initialization completed successfully');
+        CometChat.login(this.storage.getUserID(), COMET_CHAT_AUTH_KEY).then(
+          (user) => {
+            console.log('Login Successful:', { user });
+          },
+          error => {
+            console.log('Login failed with exception:', { error });
+          }
+        );
+      },
+      error => {
+        console.log('Initialization failed with error:', error);
+      }
+    );
+  }
+
+  updateUserPushToken(){
+    this.apiService.updateUser(this.storage.getUserID(), {"pushtoken":localStorage.getItem("pushtoken")}).subscribe((data) => {
+    }, (error) => {
+    });
   }
 
 }
