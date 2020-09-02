34dd2b37435556e09ba33a81d43bc199b3fb568d
Rachna Khokhar <rachnakhokhar@gmail.com>
2020-09-02 11:44:21 +0530

    Added server details

--- src/app/contants.ts
+++ src/app/contants.ts
@@ -1,6 +1,26 @@
+//Live Server
 // export const BaseUrl = 'http://ec2-3-17-28-7.us-east-2.compute.amazonaws.com:1337';
-// export const BaseUrl = 'http://localhost:1337';
-export const BaseUrl = 'https://testorbit.wattmonk.com/api';
+// export const COMETCHAT_CONSTANTS = {
+//     APP_ID: '190385dcec51285',
+//     REGION: 'US',
+//     API_KEY: '5cafae1939d4fc620698c50ae3f25e727fc90213'
+// }
+
+//Test Server
+// export const BaseUrl = 'https://testorbit.wattmonk.com/api';
+// export const COMETCHAT_CONSTANTS = {
+//     APP_ID: '22738c62a78b107',
+//     REGION: 'US',
+//     API_KEY: '3afc04a7495edb03f4c7c802096a954faf7e3a27'
+// }
+
+//Development Server
+export const BaseUrl = 'https://devspace.wattmonk.com/api';
+export const COMETCHAT_CONSTANTS = {
+    APP_ID: '2145560cac03137',
+    REGION: 'US',
+    API_KEY: '83ac811da8283c9e235ab912bf7a6213c207dd4d'
+}
 export enum ROLES {
     SuperAdmin = 4,
     ContractorSuperAdmin = 6,
@@ -12,8 +32,3 @@ export enum ROLES {
     Analyst = 10
 }
 
-export const COMETCHAT_CONSTANTS = {
-    APP_ID: '2145560cac03137',
-    REGION: 'US',
-    API_KEY: '83ac811da8283c9e235ab912bf7a6213c207dd4d'
-}
