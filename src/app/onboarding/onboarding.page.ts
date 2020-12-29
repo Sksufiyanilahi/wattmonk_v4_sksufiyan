67e97415e1255b36ea89af3474fa2503690d1631
Navjot <navjotsingh1795@gmail.com>
2020-12-29 19:33:04 +0530

    logo

--- src/app/onboarding/onboarding.page.ts
+++ src/app/onboarding/onboarding.page.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Renderer, Renderer2, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, Renderer, Renderer2, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiService } from '../api.service';
 import { StorageService } from '../storage.service';
+import { UtilitiesService } from '../utilities.service';
 //import { Slides } from 'ionic-angular';
 
 @Component({
@@ -16,6 +17,11 @@ export class OnboardingPage implements OnInit {
 
   }
 
+  @ViewChild('fileInput',{static:false}) el: ElementRef;
+  logo: any ;
+  editFile: boolean = true;
+  removeUpload: boolean = false;
+
   buffer:any;
   value=0.25;
 
@@ -27,18 +33,26 @@ export class OnboardingPage implements OnInit {
   radioValues:any;
   teamMember=[];
   userId: string;
+  blob: Blob;
+  fileName: any;
  
 
   constructor(public renderer:Renderer,
               private router:Router,
               private formBuilder: FormBuilder,
               private storage:StorageService,
-              private apiService: ApiService) {
+              private apiService: ApiService,
+              private cd:ChangeDetectorRef,
+              private utils:UtilitiesService
+              
+              ) {
                 this.firstFormGroup = this.formBuilder.group({
                   usertype : new FormControl(null),
                   billingaddress:new FormControl(null),
                   company:new FormControl(null),
-                  ispaymentmodeprepay:new FormControl(null)
+                  logo:new FormControl(null),
+                  ispaymentmodeprepay:new FormControl(null),
+                  registrationnumber:new FormControl(null)
                 })
                 this.secondFormGroup = this.formBuilder.group({
                     designcompletednotification:new FormControl(false),
@@ -80,7 +94,9 @@ this.buffer= this.value + 0.25;
         usertype:res.usertype,
         billingaddress:res.billingaddress,
         company:res.company,
-        ispaymentmodeprepay:res.ispaymentmodeprepay
+        ispaymentmodeprepay:res.ispaymentmodeprepay,
+        registrationnumber:res.registrationnumber,
+        logo:res.logo
       })
       this.secondFormGroup.patchValue({
         designcompletednotification:res.designcompletednotification,
@@ -131,7 +147,7 @@ this.buffer= this.value + 0.25;
     
     this.apiService.updateUser(this.userId,this.firstFormGroup.value).subscribe((res)=>{
       console.log('updated',res);
-      
+      this.updateLogo();
     })
   }
 
@@ -190,6 +206,43 @@ this.buffer= this.value + 0.25;
     console.log(this.secondFormGroup.value);
     this.apiService.updateUser(this.userId,this.secondFormGroup.value).subscribe((res)=>{
       console.log('updated',res);
+      
+    })
+  }
+
+  uploadFile(event) {
+    this.fileName= event.target.files[0].name;
+    console.log(this.fileName);
+    
+    let reader = new FileReader(); // HTML5 FileReader API
+    let file = event.target.files[0];
+    if (event.target.files && event.target.files[0]) {
+      reader.readAsDataURL(file);
+
+      // When file uploads set it to file formcontrol
+      reader.onload = () => {
+        this.logo = reader.result;
+        this.blob= this.utils.b64toBlob(this.logo);
+        console.log(this.blob);
+        
+        this.firstFormGroup.patchValue({
+          logo: this.fileName
+        });
+
+        console.log(this.firstFormGroup.value);
+        
+        this.editFile = false;
+        this.removeUpload = true;
+      }
+      // ChangeDetectorRef since file is loading outside the zone
+      this.cd.markForCheck();        
+    }
+  }
+
+  updateLogo(){
+
+    this.apiService.uploadlogo(this.blob,this.fileName).subscribe(res=>{
+      console.log(res);
       
     })
   }
