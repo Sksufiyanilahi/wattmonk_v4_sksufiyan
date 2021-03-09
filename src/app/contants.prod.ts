4cb268aca2746e7f550e3bfef3d66904da930b35
anujdahiya068 <anujdahiya068@gmail.com>
2021-03-09 16:24:16 +0530

    bug

--- src/app/contants.prod.ts
+++ src/app/contants.prod.ts
@@ -11,17 +11,17 @@
 // export const PlatformUpdateUrl =  'https://webops.wattmonk.com/api';
 // export const PlatformUpdateUrl =  ' http://ec2-52-15-220-175.us-east-2.compute.amazonaws.com:1802/';
 
-export const BaseUrl = 'http://ec2-52-15-220-175.us-east-2.compute.amazonaws.com:1802/';
-export const PlatformUpdateUrl = 'http://ec2-52-15-220-175.us-east-2.compute.amazonaws.com:1802/';
-export const intercomId = 'o52f08q6';
-export const COMETCHAT_CONSTANTS = {
-    APP_ID: '190385dcec51285',
-    REGION: 'US',
-    API_KEY: '5cafae1939d4fc620698c50ae3f25e727fc90213',
-    REST_API_KEY: "4f441010f9ace69fc5e2471c20e9f1c21ca56402"
-}
-export const version='1.1.3'
-export const Mixpanel_CONSTANTS ='e4b30b18d61b5abe078c2719911858cb';
+// export const BaseUrl = 'http://ec2-52-15-220-175.us-east-2.compute.amazonaws.com:1802/';
+// export const PlatformUpdateUrl = 'http://ec2-52-15-220-175.us-east-2.compute.amazonaws.com:1802/';
+// export const intercomId = 'o52f08q6';
+// export const COMETCHAT_CONSTANTS = {
+//     APP_ID: '190385dcec51285',
+//     REGION: 'US',
+//     API_KEY: '5cafae1939d4fc620698c50ae3f25e727fc90213',
+//     REST_API_KEY: "4f441010f9ace69fc5e2471c20e9f1c21ca56402"
+// }
+// export const version='1.1.3'
+// export const Mixpanel_CONSTANTS ='e4b30b18d61b5abe078c2719911858cb';
 
 // Test Server
 // export const PlatformUpdateUrl = 'https://testorbit.wattmonk.com/api';
@@ -36,16 +36,16 @@ export const Mixpanel_CONSTANTS ='e4b30b18d61b5abe078c2719911858cb';
 // export const Mixpanel_CONSTANTS ='e4b30b18d61b5abe078c2719911858cb';
 
 //Development Server
-// export const PlatformUpdateUrl = 'https://devspace.wattmonk.com/api';
-// export const BaseUrl = 'https://devspace.wattmonk.com/api/';
-// export const intercomId = 'c6w73e4e';
-// export const COMETCHAT_CONSTANTS = {
-//    APP_ID: '2145560cac03137',
-//    REGION: 'US',
-//    API_KEY: '83ac811da8283c9e235ab912bf7a6213c207dd4d'
-// }
-// export const version='1.1.3'
-// export const Mixpanel_CONSTANTS ='e4b30b18d61b5abe078c2719911858cb';
+export const PlatformUpdateUrl = 'https://devspace.wattmonk.com/api';
+export const BaseUrl = 'https://devspace.wattmonk.com/api/';
+export const intercomId = 'c6w73e4e';
+export const COMETCHAT_CONSTANTS = {
+   APP_ID: '2145560cac03137',
+   REGION: 'US',
+   API_KEY: '83ac811da8283c9e235ab912bf7a6213c207dd4d'
+}
+export const version='1.1.3'
+export const Mixpanel_CONSTANTS ='e4b30b18d61b5abe078c2719911858cb';
 
 export enum ROLES {
   SuperAdmin = 4,
