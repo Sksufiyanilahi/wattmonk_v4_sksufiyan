7629dec3b8b9068d663f553fcefa3e88b7fd42f2
Rachna Khokhar <rachnakhokhar@gmail.com>
2021-04-29 12:41:45 +0530

    Removed usage of latitude and longitude

--- src/app/surveyoroverview/newsurveys/newsurveys.component.ts
+++ src/app/surveyoroverview/newsurveys/newsurveys.component.ts
@@ -301,14 +301,14 @@ export class NewsurveysComponent implements OnInit {
     this.apiService.updateSurveyForm(postData, surveyData.id).subscribe(res => {
 
     })
-    this.router.navigate(['/camera/' + surveyData.id + '/' + surveyData.jobtype + '/' + surveyData.city + '/' + surveyData.state + '/' + surveyData.latitude + '/' + surveyData.longitude]);
+    this.router.navigate(['/camera/' + surveyData.id + '/' + surveyData.jobtype + '/' + surveyData.city + '/' + surveyData.state]);
 
 
   }
 
   resumeSurvey(surveyData, event) {
     event.stopPropagation();
-    this.router.navigate(['/camera/' + surveyData.id + '/' + surveyData.jobtype + '/' + surveyData.city + '/' + surveyData.state + '/' + surveyData.latitude + '/' + surveyData.longitude]);
+    this.router.navigate(['/camera/' + surveyData.id + '/' + surveyData.jobtype + '/' + surveyData.city + '/' + surveyData.state]);
   }
 
   gotoActivity(surveyData, event) {
