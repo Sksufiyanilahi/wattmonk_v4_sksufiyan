3aa77ead6351baaf3d261635464b947e933b745f
ahujasahil <sahilahuja2233@gmail.com>
2021-05-13 17:24:29 +0530

    Added charts in Design-Tracker

--- src/app/design-tracker/design-tracker.page.ts
+++ src/app/design-tracker/design-tracker.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit,  ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
-import {ChartDataSets, ChartOptions, ChartType} from 'chart.js';
-import {Color, Label} from 'ng2-charts';
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'app-design-tracker',
@@ -9,11 +8,11 @@ import {Color, Label} from 'ng2-charts';
   styleUrls: ['./design-tracker.page.scss'],
 })
 export class DesignTrackerPage implements OnInit {
-  @ViewChild("barCanvas") barCanvas: ElementRef;
-  @ViewChild("doughnutCanvas") doughnutCanvas: ElementRef;
-  @ViewChild("lineCanvas") lineCanvas: ElementRef;
+  @ViewChild('barChart') barChart;
+  @ViewChild("doughnutCanvas") doughnutCanvas;
+  
 
-  private barChart: Chart;
+  // private barChart: Chart;
    slideOpts = {
     slidesPerView: 1,
     initialSlide: 1,
@@ -21,6 +20,9 @@ export class DesignTrackerPage implements OnInit {
   }
   month: boolean = true;
   year: boolean = false;
+  bars: any;
+  Doughs:any
+  colorArray: any;
 
   constructor( private router:Router,) { }
 
@@ -35,14 +37,83 @@ export class DesignTrackerPage implements OnInit {
   ToogleMonth(){
     this.month=true;
     this.year = false;
+    
   }
 
   ToogleYear(){
     this.year = true;
     this.month= false;
-    this.router.navigate(["/design-tracker/monthly-status"])
   }
+
+  ionViewDidEnter() {
+    this.createBarChart();
+    this.createDoughChart();
+  }
+
+  createBarChart() {
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['2018', '2019','2020','2021'],
+        datasets: [{
+          barPercentage: 0.8,
+          barThickness: 27.5,
+          stack:'base',
+          label: 'Work Analytics',
+          data: [400, 600, 250,800],
+          backgroundColor: '#EDC773', // array should have same number of elements as number of dataset
+          borderColor: '#EDC773',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+             
+            }
+          }]
+        },
+        plugins: {
+          datalabels: {
+            anchor: 'end',
+            align: 'end',
+            font: {
+              size: 10,
+            }
+          }
+        }
+      }
+    });
+  }
+
+  createDoughChart(){
+    this.Doughs = new Chart(this.doughnutCanvas.nativeElement, {
+      type: "doughnut",
+      data: {
+        
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5],
+            backgroundColor: [
+              "rgba(255, 206, 86, 0.2)",
+              "rgb(252, 235, 182,0.2)",
+              "rgb(255, 165, 91,0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            hoverBackgroundColor: [ "#FFCE56", "#EDC773", "#fcad80", "#FFCE56"]
+          }
+        ]
+      }
+    });
+  }
+  
+
+  
+}
 
   
 
-}
+
