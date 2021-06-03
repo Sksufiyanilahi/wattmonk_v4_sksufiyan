d540e1d1ee60d601ae9ddc13391abc14557eedd0
Rachna Khokhar <rachnakhokhar@gmail.com>
2021-06-04 00:54:11 +0530

    Production ReleaseV1.7.6(2)

--- src/app/startsurvey/startsurvey.page.ts
+++ src/app/startsurvey/startsurvey.page.ts
@@ -295,8 +295,8 @@ export class StartsurveyPage implements OnInit {
         this.platformname = 'web';
       }
 
-      // this.loadSurveyJSON('pvsurveyjson');
-      this.loadLocalJSON();
+      this.loadSurveyJSON('pvsurveyjson');
+      // this.loadLocalJSON();
       this.getCurrentCoordinates();
     } catch (error) {
       console.log("ngOnInit---" + error);
