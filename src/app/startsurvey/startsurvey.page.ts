06ed5cee0457f792c9cc95e7d487d9f5194235ad
himanshu <himanshu@wattmonk.com>
2021-06-17 15:51:41 +0530

    creator parent id and delivery date

--- src/app/startsurvey/startsurvey.page.ts
+++ src/app/startsurvey/startsurvey.page.ts
@@ -1244,6 +1244,9 @@ export class StartsurveyPage implements OnInit {
   }
 
   savedetailsformdata() {
+    let tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate());
+    let d_date = tomorrow.toISOString();
     try {
       this.utilitieservice.setLoadingMessage('Saving form data');
       this.saveintermediatesurveydata();
@@ -1262,6 +1265,7 @@ export class StartsurveyPage implements OnInit {
       data['status'] = 'completed';
       data['latitude'] = this.latitude;
       data['longitude'] = this.longitude;
+      data['deliverydate']=d_date
       this.apiService.updateSurveyForm(data, this.surveyid).subscribe((response) => {
         this.utilitieservice.hideLoading().then(() => {
           this.utilitieservice.showSuccessModal('Survey completed successfully').then((modal) => {
